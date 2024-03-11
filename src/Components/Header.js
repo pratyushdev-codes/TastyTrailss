c32547a664e8ff85b57aa9b7f047dd96ffccb5cc
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-11 15:37:45 +0530

    Hero sec

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Styles.css'
 
-function Header
-() {
+function Hero() {
   return (
-    <div>Header
-      <div class="hero">
-  <div class="hero__bg">
-    <picture>
-      <img src="./images/Header.webp"/>
-    </picture>
-  </div>
+    <div className="hero">
+      <div className="hero__bg">
+        <picture>
+          <img src="./images/Header.webp" alt="Background"/>
+        </picture>
+      </div>
 
-  <div class="hero__cnt">
-    
-    <h1>Savor the Flavor</h1>
-    <h7>Experience the best of local and international cuisine at Tasty Trailss.</h7>
-  </div>
-</div>
-</div>
-
-   
-  )
+      <div className="hero__cnt">
+        <h1 style={{fontSize:"70px", }}><span style={{color:"#E0A01A"}}>Savour</span> the flavour</h1>
+        <p style={{fontSize:"20px"}}>Discover a world of delicious tastes.</p>
+        <button style={{backgroundColor:"blue", borderRadius:"20px"}}>Explore Now</button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Hero;
